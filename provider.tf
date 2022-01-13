57f43be1995b254ca9b5b4afc296085d4668e28d
EC2 Default User <ec2-user@ip-172-31-82-1.ec2.internal>
2022-01-13 11:22:46 +0000

    committing Terraform extra files

---
+++ provider.tf
@@ -0,0 +1,12 @@
+provider "aws" {
+    region = var.aws_region
+}
+
+terraform {
+    backend "s3" {
+        bucket = "shark-is-in-action"
+        key = "terraform.tfstate"
+        region = "us-east-1"
+        dynamodb_table = "terraform"
+    }
+}

91978160d778b92aa26497fc7555531750b1aca6
EC2 Default User <ec2-user@ip-172-31-82-1.ec2.internal>
2022-01-17 07:47:10 +0000

    Classic LB replaced with Application LB, Auto Scaling Group, Target Group, Listener created

--- web-ec2.tf
+++ web-ec2.tf
@@ -3,6 +3,7 @@ locals {
         Environment = terraform.workspace
     }
     web_tags = merge(var.web_instance_tag, local.env_tag)
+
 }
 
 resource "aws_instance" "web" {
@@ -20,7 +21,8 @@ resource "aws_instance" "web" {
 }
 
 resource "aws_security_group" "web_sg" {
-    name = "web_sg"adding elb
+    name = "web_sg"
+    description = "Allow traffic"
     vpc_id = aws_vpc.demo_vpc.id
 
     ingress {

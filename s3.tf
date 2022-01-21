04e0fdc82c9fbaef842d8c91d2ed1245160fd6b5
kzs61 <kzs613490@gmail.com>
2022-01-21 10:01:16 +0000

    Added changes for ALB Access Logging, s3 bucket for ALB access logs

--- s3.tf
+++ s3.tf
@@ -1,3 +1,10 @@
+data "aws_caller_identity" "account-id" {}
+
+locals {
+  current_account = data.aws_caller_identity.account-id.account_id
+}
+
+
 resource "aws_s3_bucket" "demo_bucket" {
   bucket = var.s3_bucket
   acl    = "private"
@@ -6,4 +13,25 @@ resource "aws_s3_bucket" "demo_bucket" {
     Name        = var.s3_bucket
     Environment = terraform.workspace
   }
+}
+
+resource "aws_s3_bucket" "alb_access_log" {
+  bucket = var.alb_access_log_s3_bucket
+  policy = data.template_file.alb_access_logpolicy.rendered
+  acl    = "private"
+  
+  # force_destroy = true # destroys the bucket and objects
+  
+  tags = {
+    Name        = var.alb_access_log_s3_bucket
+    Environment = terraform.workspace
+  }
+}
+
+data "template_file" "alb_access_logpolicy" {
+    template = file("scripts/iam/elb-access-logging.json")
+    vars = {
+        access_logs_bucket = var.alb_access_log_s3_bucket
+        policy_account_id = local.current_account
+    }
 }
